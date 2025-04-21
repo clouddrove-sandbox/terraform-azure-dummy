3db087e61fd7cc463b88942544283b583d13ceef
Ravi Malvia <ravi.malvia@clouddrove.com>
2025-04-21 19:58:26 +0530

    fix: test the  code

--- examples/complete/example.tf
+++ examples/complete/example.tf
@@ -62,7 +62,7 @@ module "subnet" {
   virtual_network_name = module.vnet.vnet_name
   service_endpoints    = ["Microsoft.Storage"]
   #subnet
-  subnet_names    = ["subnet1"]
+  subnet_names    = ["subnet2"]
   subnet_prefixes = ["10.0.1.0/24"]
 }
 
